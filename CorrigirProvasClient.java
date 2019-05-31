856e41cd57669246e5993deb6d3e182d5b61db14
Murilo Andrade <muriloandradealmeida@gmail.com>
2019-05-31 00:42:38 -0300

    try/catch with pctVai and pctVeio added

--- CorrigirProvasClient.java
+++ CorrigirProvasClient.java
@@ -1,10 +1,14 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.util.Scanner;
 
 public class CorrigirProvasClient {
 
     public static void main(String[] args) {
         DatagramSocket socket;
+        int portDst = 6789;
         Scanner input = new Scanner(System.in);
         
         System.out.print("Informe a quantidade de questões: ");
@@ -24,5 +28,25 @@ public class CorrigirProvasClient {
         }
 
         System.out.println("Respostas como vão pro servidor: " + respostas);
+
+        try {
+            socket = new DatagramSocket();
+            byte[] message = respostas.getBytes();
+            InetAddress host = InetAddress.getByName("localhost");
+            
+            DatagramPacket pctVai = new DatagramPacket(message, message.length, host, portDst);
+            socket.setSoTimeout(30000);
+            socket.send(pctVai);
+            System.out.println("Prova enviada para correção! Aguarde resposta.");
+            
+            byte[] buffer = new byte[1024];
+            DatagramPacket pctVeio = new DatagramPacket(buffer, buffer.length);
+            socket.receive(pctVeio);
+            Thread.sleep(10000);
+            System.out.println("Resposta:" + new String(pctVeio.getData()).trim());
+            socket.close();
+        } catch (IOException | InterruptedException e) {
+            System.err.println("Erro: " + e.getMessage());
+        }
     }
 }
